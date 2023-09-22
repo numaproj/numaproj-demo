bcd83174f2bde66110992054840da08834bcfd29
Saravanan Balasubramanian <sarabala1979@gmail.com>
2023-09-22 10:48:54 -0700

    update
    Signed-off-by: Saravanan Balasubramanian <sarabala1979@gmail.com>

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ strings=(
 )
 
 for fish in "${strings[@]}"; do
-    make image FISH=${fish} DOCKER_PUSH=${DOCKER_PUSH} IMAGE_NAMESPACE=quay.io/numaio
-    make image FISH=${fish} ERROR_RATE=60 DOCKER_PUSH=${DOCKER_PUSH} IMAGE_NAMESPACE=quay.io/numaio
-    make image FISH=${fish} LATENCY=2 DOCKER_PUSH=${DOCKER_PUSH} IMAGE_NAMESPACE=quay.io/numaio
+    make image FISH=${fish} DOCKER_PUSH=${DOCKER_PUSH} IMAGE_NAMESPACE=${IMAGE_NAMESPACE}
+    make image FISH=${fish} ERROR_RATE=60 DOCKER_PUSH=${DOCKER_PUSH} IMAGE_NAMESPACE=${IMAGE_NAMESPACE}
+    make image FISH=${fish} LATENCY=2 DOCKER_PUSH=${DOCKER_PUSH} IMAGE_NAMESPACE=${IMAGE_NAMESPACE}
 done
