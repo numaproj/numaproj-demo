102ddd5c39c1fc453e18760731e3dbe1904e7c8d
Derek Wang <whynowy@gmail.com>
2024-03-04 22:58:04 -0800

    refine

--- dooreats/main.go
+++ dooreats/main.go
@@ -57,10 +57,10 @@ var (
 //	  ]
 //	}
 //
-// Enrich the order info to add area
-func enrich(_ context.Context, keys []string, d mapper.Datum) mapper.Messages {
+// Enrich the order info to add restaurant name and price for each dish
+func enrich(_ context.Context, keys []string, msg mapper.Datum) mapper.Messages {
 	results := mapper.MessagesBuilder()
-	order := d.Value()
+	order := msg.Value()
 	restaurantID := gjson.GetBytes(order, "restaurant_id").String()
 	restaurantName := gjson.Get(restaurants, fmt.Sprintf("%s.name", restaurantID)).String()
 	enrichedOrder, _ := sjson.SetBytes(order, "restaurant_name", restaurantName)
@@ -69,19 +69,19 @@ func enrich(_ context.Context, keys []string, d mapper.Datum) mapper.Messages {
 		price := gjson.Get(restaurants, fmt.Sprintf(`%s.menu.#(id=="%s").price`, restaurantID, dishID)).Float()
 		enrichedOrder, _ = sjson.SetBytes(enrichedOrder, fmt.Sprintf("dishes.%v.price", i), price)
 	}
-	log.Println("Enriched order: ", string(enrichedOrder))
 	results = append(results, mapper.NewMessage(enrichedOrder).WithKeys([]string{restaurantName}))
 	return results
 }
 
-// Count how many data comes for a particular keys combination
-func aggregate(_ context.Context, keys []string, inputCh <-chan reducer.Datum, md reducer.Metadata) reducer.Messages {
+// Aggregate the order info to count the number of orders and calculate the total amount
+func aggregate(_ context.Context, keys []string, msgCh <-chan reducer.Datum,
+	md reducer.Metadata) reducer.Messages {
 	restaurantName := keys[0]
 	var orderCounter = 0
 	var amount = float64(0)
-	for d := range inputCh {
+	for msg := range msgCh {
 		orderCounter++
-		order := d.Value()
+		order := msg.Value()
 		for _, dish := range gjson.GetBytes(order, "dishes").Array() {
 			price := dish.Get("price").Float()
 			quantity := dish.Get("quantity").Int()
@@ -97,7 +97,6 @@ func aggregate(_ context.Context, keys []string, inputCh <-chan reducer.Datum, m
 	result, _ = sjson.Set(result, "order_count", orderCounter)
 	result, _ = sjson.Set(result, "total_amount", amount)
 
-	log.Println("Result: ", result)
 	return messages.Append(reducer.NewMessage([]byte(result)))
 }
 
